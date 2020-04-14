a614ee39ce88a13c40dab9f9b9fb335b4690b36d
D3ad5h0t <alexgl007@mail.ru>
2020-04-14 15:42:03 +1000

    First commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Информация о поекте</h1>
+                <p className="lead">Данное приложение создано для изучения автором Хуков в фреймворке React</p>
+                <p className="lead">Версия проекта: <strong>1.0.0</strong></p>
+            </div>
+        </div>
+    )
+};
